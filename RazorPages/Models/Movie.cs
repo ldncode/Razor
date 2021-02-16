7e41b26794a83783cf9656306b77d673feb597cc
LDN <ledenevdn@gmail.com>
2021-02-16 14:55:38 +0700

    Page update

--- RazorPages/Models/Movie.cs
+++ RazorPages/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPages.Models
 {
@@ -8,10 +9,12 @@ namespace RazorPages.Models
 
         public int ID { get; set; }
         public string Title { get; set; }
-
+       
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
